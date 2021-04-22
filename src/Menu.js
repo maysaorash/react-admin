6b3265887fc9691f518096ea6e4e3f1701c99606
Maysa Orazova <[maysaorash@gmail.com]>
2021-04-22 16:15:31 -0400

    second commit

--- src/Menu.js
+++ src/Menu.js
@@ -6,21 +6,30 @@ export default class Menu extends Component {
           <div>
   <aside className="main-sidebar sidebar-dark-primary elevation-4">
     {/* Brand Logo */}
+   <div>
     <a href="index3.html" className="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} />
-      <span className="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} /> 
+       {/* <img src="../assets/images/logo.png" class="img-fluid rounded-normal" alt=""></img> */}
+      <span className="brand-text font-weight-light" style={{ color: 'red' }}>STREAMIT </span> 
+     
+     {/* <ul className="navbar-nav"> */}
+      <div className="nav-item">
+        <a className="nav-link" data-widget="pushmenu" href="#" style={{ color: 'red' }}><i className="fas fa-bars" /></a>
+      </div>
+      </a>
+      </div>
+     
     {/* Sidebar */}
     <div className="sidebar">
       {/* Sidebar user panel (optional) */}
-      <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+      {/* <div className="user-panel mt-3 pb-3 mb-3 d-flex">
         <div className="image">
           <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
         </div>
         <div className="info">
-          <a href="#" className="d-block">Alexander Pierce</a>
+          <a href="#" className="d-block">STREAMIT</a>
         </div>
-      </div>
+      </div> */}
       {/* Sidebar Menu */}
       <nav className="mt-2">
         <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -30,7 +39,7 @@ export default class Menu extends Component {
             <a href="#" className="nav-link active">
               <i className="nav-icon fas fa-tachometer-alt" />
               <p>
-                Dashboard
+                Visit Site
                 <i className="right fas fa-angle-left" />
               </p>
             </a>
@@ -38,19 +47,7 @@ export default class Menu extends Component {
               <li className="nav-item">
                 <a href="./index.html" className="nav-link active">
                   <i className="far fa-circle nav-icon" />
-                  <p>Dashboard v1</p>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="./index2.html" className="nav-link">
-                  <i className="far fa-circle nav-icon" />
-                  <p>Dashboard v2</p>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="./index3.html" className="nav-link">
-                  <i className="far fa-circle nav-icon" />
-                  <p>Dashboard v3</p>
+                  <p>Dashboard </p>
                 </a>
               </li>
             </ul>
@@ -59,8 +56,8 @@ export default class Menu extends Component {
             <a href="pages/widgets.html" className="nav-link">
               <i className="nav-icon fas fa-th" />
               <p>
-                Widgets
-                <span className="right badge badge-danger">New</span>
+                Rating
+                {/* <span className="right badge badge-danger">New</span> */}
               </p>
             </a>
           </li>
